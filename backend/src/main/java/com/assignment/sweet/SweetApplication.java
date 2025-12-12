745674b459ad6d7395cfd15580e17d12f66e015f
Amitabha Nath <supersaiyan2k03@gmail.com>
2025-12-12 21:08:02 +0530

    initial commit

---
+++ backend/src/main/java/com/assignment/sweet/SweetApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.sweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/assignment/sweet/SweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.sweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
